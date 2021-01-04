d3c31c3ceddd17b441b92501319c07dedbd33ac1
Kristofer <skipperk@interia.pl>
2021-01-04 11:41:09 +0100

    Dodaj pliki projektów.

---
+++ Moja pierwsza gra/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moja_pierwsza_gra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
